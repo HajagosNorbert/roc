822800b702fc9e37b753ff1afce2796086710c60
HajagosNorbert <hajagosnorbi@gmail.com>
2023-11-16 14:25:11 +0100

    preping to make changes

--- crates/compiler/test_mono/src/tests.rs
+++ crates/compiler/test_mono/src/tests.rs
@@ -249,40 +249,6 @@ fn verify_procedures<'a>(
         ));
     }
 }
-#[mono_test]
-fn norbi_try() {
-    indoc!(
-        r#"
-        ConsList a : [ Nil, Cons { first: a, rest: ConsList a } ]
-
-        map : ConsList a, (a -> b) -> ConsList b
-        map = \list, f ->
-            when list is
-                Nil -> Nil
-                Cons { first, rest } -> Cons { first: f first, rest: map rest f }
-
-        map (Cons { first: 1, rest: Nil }) Num.neg
-        "#
-    )
-}
-#[mono_test]
-fn trmc_map_conslist() {
-    indoc!(
-        r#"
-        ConsList a : [Nil, Cons a (ConsList a)]
-
-        map : ConsList a, (a -> b) -> ConsList b
-        map = \list, f ->
-            when list is
-                Cons x xx -> 
-                    rest = map xx f
-                    Cons (f x) rest
-                Nil -> Nil
-
-        map (Cons 1 (Cons 2 Nil)) Num.neg
-        "#
-    )
-}
 
 #[mono_test]
 fn ir_int_literal() {
