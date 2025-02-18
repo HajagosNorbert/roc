e2ce631c898252560afafbebb881c80ebbc2c3f1
Norbert Hajagos <hajagosnorbi@gmail.com>
2025-02-18 21:33:32 +0100

    serialization exploration

--- src/base.zig
+++ src/base.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+const testing = std.testing;
 pub const Ident = @import("base/Ident.zig");
 pub const Module = @import("base/Module.zig");
 pub const Region = @import("base/Region.zig");
@@ -54,3 +56,81 @@ pub const Literal = union(enum) {
         Float: Float,
     };
 };
+
+pub fn serializeLiteral(lit: Literal, strings: *StringLiteral.Store, writer: std.io.AnyWriter) !void {
+    switch (lit) {
+        .Bool => |b| {
+            if (b) {
+                try writer.writeAll("true");
+            } else {
+                try writer.writeAll("false");
+            }
+        },
+        .Crash => |idx| {
+            try writer.print("crash: \"{s}\"", .{strings.get(idx)});
+        },
+        .Str => |idx| {
+            try writer.print("\"{s}\"", .{strings.get(idx)});
+        },
+        .Int => |i| {
+            switch (i) {
+                inline else => |num| {
+                    const tag_name = @tagName(@as(std.meta.Tag(Literal.Int), i));
+                    var tag_name_lc_buff: [4]u8 = undefined;
+                    const tag_name_lc = std.ascii.lowerString(&tag_name_lc_buff, tag_name);
+                    try writer.print("{d}{s}", .{ num, tag_name_lc });
+                },
+            }
+        },
+        .Float => |f| {
+            switch (f) {
+                .F32 => |num| try writer.print("{d:.0}f32", .{num}),
+                .F64 => |num| try writer.print("{d:.0}f64", .{num}),
+                .Dec => |_| try writer.print("todo", .{}),
+            }
+        },
+    }
+}
+
+test "serialize" {
+    var buff = std.ArrayList(u8).init(testing.allocator);
+    defer buff.deinit();
+
+    var str_interner = StringLiteral.Store.init(testing.allocator);
+    defer str_interner.deinit();
+
+    var str_lit = "testing".*;
+    const str_lit_idx = str_interner.insert(&str_lit);
+
+    var crash_lit = "crashed".*;
+    const crash_lit_idx = str_interner.insert(&crash_lit);
+
+    const cases = .{
+        .{ .lit = Literal{ .Bool = true }, .expected = "true" },
+        .{ .lit = Literal{ .Bool = false }, .expected = "false" },
+
+        .{ .lit = Literal{ .Int = .{ .U8 = 3 } }, .expected = "3u8" },
+        .{ .lit = Literal{ .Int = .{ .I8 = 3 } }, .expected = "3i8" },
+        .{ .lit = Literal{ .Int = .{ .U16 = 3 } }, .expected = "3u16" },
+        .{ .lit = Literal{ .Int = .{ .I16 = 3 } }, .expected = "3i16" },
+        .{ .lit = Literal{ .Int = .{ .U32 = 3 } }, .expected = "3u32" },
+        .{ .lit = Literal{ .Int = .{ .I32 = 3 } }, .expected = "3i32" },
+        .{ .lit = Literal{ .Int = .{ .U64 = 3 } }, .expected = "3u64" },
+        .{ .lit = Literal{ .Int = .{ .I64 = 3 } }, .expected = "3i64" },
+        .{ .lit = Literal{ .Int = .{ .U128 = 3 } }, .expected = "3u128" },
+        .{ .lit = Literal{ .Int = .{ .I128 = 3 } }, .expected = "3i128" },
+
+        .{ .lit = Literal{ .Float = .{ .F32 = 3 } }, .expected = "3f32" },
+        .{ .lit = Literal{ .Float = .{ .F64 = 3 } }, .expected = "3f64" },
+        .{ .lit = Literal{ .Float = .{ .Dec = 3 } }, .expected = "todo" },
+
+        .{ .lit = Literal{ .Str = str_lit_idx }, .expected = "\"testing\"" },
+        .{ .lit = Literal{ .Crash = crash_lit_idx }, .expected = "crash: \"crashed\"" },
+    };
+
+    inline for (cases) |case| {
+        try serializeLiteral(case.lit, &str_interner, buff.writer().any());
+        try testing.expectEqualStrings(case.expected, buff.items);
+        buff.clearRetainingCapacity();
+    }
+}
